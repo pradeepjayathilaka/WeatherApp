cb4a65e96318ca53ac7412f47f22a17989c9dab6
pradeepjayathilaka <pradeepjayathilaka06@gmail.com>
2025-03-20 20:46:29 +0530

    Refactor main app structure; add HomePage and Weather model; load environment variables

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,19 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final double condition;
+  final String description;
+  final double pressure;
+  final double humidity;
+  final double windSpeed;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.condition,
+    required this.description,
+    required this.pressure,
+    required this.humidity,
+    required this.windSpeed,
+  });
+}
